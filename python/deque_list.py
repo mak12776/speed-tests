973558cfdf5395e0484dabde1ec53e4085f58e95
Mohammad Amin Khakzadan <mak12776@gmail.com>
2019-04-29 21:53:03 +0430

    update deque_list.py

--- python/deque_list.py
+++ python/deque_list.py
@@ -1,15 +1,15 @@
 
 import collections
 
-def func1():
+def func1(size):
     result = []
-    for i in range(1000):
+    for i in range(size):
         result.append(1)
     return result
 
-def func2():
+def func2(size):
     result = collections.deque()
-    for i in range(1000):
+    for i in range(size):
         result.append(1)
     return list(result)
 
